4d6e1c9dc09d9c52afa5de95f2240c2837358b75
LetoSum <smile-on-mail-tanya@mail.ru>
2017-01-28 21:32:41 +0300

    guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    	this.min = min + 1;
+    	this.max = max - 1;
     }
 
     guess() {
-
+    	return Math.round((this.max - this.min)/2 + this.min);
     }
 
     lower() {
-
+    	this.max = this.guess() - 1;
     }
 
     greater() {
-
+    	this.min = this.guess() + 1;
     }
 }
 
